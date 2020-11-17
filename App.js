484abe3a294144745cd8242f6796d2bd75dd417a
jhonatan <jhonatanferreira13@hotmail.com>
2020-11-17 18:18:09 -0300

    Firs Commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  NativeModules,
+  NativeEventEmitter,
+  Text,
+} from 'react-native';
+
+function App() {
+  const [token, setToken] = useState('Sem Token');
+  useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(NativeModules.Wall);
+    eventEmitter.addListener('TOKEN', event => {
+      setToken(event);
+    });
+  }, []);
+  const callNativeFunction = () => NativeModules?.Wall?.startSDK();
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>{token}</Text>
+      <Button title="Iniciar Fluxo" onPress={callNativeFunction} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# PocIdWall
+Implementation of native IOS and Android SDK with React Native
